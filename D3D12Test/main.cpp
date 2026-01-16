84daf177ab0b9c281cb5b51465c694ec9d79dde6
PanaxTab <panax36@gmail.com>
2026-01-16 17:51:15 +0200

    first commit

---
+++ D3D12Test/main.cpp
@@ -0,0 +1,27 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <shellapi.h>
+
+#if defined(min)
+#undef min
+#endif
+
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif
+
+#include <wrl.h>
+
+using namespace Microsoft::WRL;
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include <d3dx12.h>
+
+int main()
+{
+	return 0;
+}
