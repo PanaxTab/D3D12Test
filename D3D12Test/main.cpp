a285fae166a438cd85c3ce5072e491bbdb4d94c5
PanaxTab <panax36@gmail.com>
2026-01-18 19:32:29 +0200

    2nd Commit

--- D3D12Test/main.cpp
+++ D3D12Test/main.cpp
@@ -21,6 +21,168 @@ using namespace Microsoft::WRL;
 
 #include <d3dx12.h>
 
+// STL Headers
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+
+// Helper functions
+#include "Helpers.h"
+
+
+// Swap Chain buffer frames
+const uint8_t numFrames = 3;
+uint32_t clientWidth = 1280;
+uint32_t clientHeight = 720;
+
+// WARP adapter
+bool useWARP = false;
+// True if DX12 objects are init
+bool g_IsInitialized = false;
+
+// Window Handler
+HWND hWnd;
+// Toggle fullscreen
+RECT winRect;
+
+// DirectX 12 Objects
+ComPtr<ID3D12Device2> device;
+ComPtr<ID3D12CommandQueue>commandQueue;
+ComPtr<IDXGISwapChain4> swapChain;
+ComPtr<ID3D12Resource> backBuffers[numFrames];
+ComPtr<ID3D12GraphicsCommandList> commandList;
+ComPtr<ID3D12CommandAllocator> commandAllocator[numFrames];
+ComPtr<ID3D12DescriptorHeap> descriptorHeap;
+
+UINT descriptorSize;
+UINT currentBufferIndex;
+
+ComPtr<ID3D12Fence> fence;
+uint64_t fenceValue = 0;
+uint64_t frameFenceValue[numFrames] = {};
+HANDLE fenceEvent;
+
+// V-sync
+bool vSync = true;
+bool tearingSupported = false;
+bool fullscreen = false;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+void parseCommandLineArguments() 
+{
+	int argc;
+	wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+
+	for (size_t i = 0; i < argc; ++i)
+	{
+		if (::wcscmp(argv[i], L"-w") == 0 || ::wcscmp(argv[i], L"--width") == 0)
+		{
+			clientWidth = ::wcstol(argv[++i], nullptr, 10);
+		}
+		if (::wcscmp(argv[i], L"-h") == 0 || ::wcscmp(argv[i], L"--height") == 0)
+		{
+			clientHeight = ::wcstol(argv[++i], nullptr, 10);
+		}
+		if (::wcscmp(argv[i], L"-warp") == 0 || ::wcscmp(argv[i], L"--warp") == 0)
+		{
+			useWARP = true;
+		}
+	}
+	::LocalFree(argv);
+}
+
+void enableDebugLayer()
+{
+#if defined(_DEBUG)
+	ComPtr<ID3D12Debug> debugInterface;
+	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+	debugInterface->EnableDebugLayer();
+#endif
+}
+
+void	registerWindowClass(HINSTANCE hInst, const wchar_t* windowClassName)
+{
+	WNDCLASSEXW windowClass = {};
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = &WndProc;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = hInst;
+	windowClass.hIcon = ::LoadIcon(hInst, NULL);
+	windowClass.hCursor = ::LoadCursor(hInst, NULL);
+	windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	windowClass.lpszMenuName = NULL;
+	windowClass.lpszClassName = windowClassName;
+	windowClass.hIconSm = ::LoadIcon(hInst, NULL);
+
+	static ATOM atom = ::RegisterClassExW(&windowClass);
+	assert(atom > 0);
+}
+
+HWND createWindow(const wchar_t* windowClassName, HINSTANCE hInst, const wchar_t* windowTitle, uint32_t width, uint32_t height)
+{
+	int screenWidth = ::GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = ::GetSystemMetrics(SM_CYSCREEN);
+
+	RECT windowRect = { 0,0,static_cast<LONG>(width),static_cast<LONG>(height) };
+	::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	int windowWidth = windowRect.right - windowRect.left;
+	int windowHeight = windowRect.bottom - windowRect.top;
+
+	int windowX = std::max<int>(0, (screenWidth - windowWidth)/2);
+	int windowY	= std::max<int>(0,(screenHeight-windowHeight)/2);
+
+	HWND hWnd = ::CreateWindowExW(
+		NULL,
+		windowClassName,
+		windowTitle,
+		WS_OVERLAPPEDWINDOW,
+		windowX,
+		windowY,
+		windowWidth,
+		windowHeight,
+		NULL,
+		NULL,
+		hInst,
+		nullptr
+	);
+
+	assert(hWnd && "Failed to create window");
+
+	return hWnd;
+}
+
+ComPtr<IDXGIAdapter4> getAdapter(bool useWARP) 
+{
+	ComPtr<IDXGIFactory4> dxgiFactory;
+	UINT createFactoryFlags = 0;
+#if defined(_DEBUG)
+	createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+	ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags,IID_PPV_ARGS(&dxgiFactory)));
+
+	ComPtr<IDXGIAdapter1> dxgiAdapter1;
+	ComPtr<IDXGIAdapter4> dxgiAdapter4;
+
+	if (useWARP)
+	{
+		ThrowIfFailed(dxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&dxgiFactory)));
+		ThrowIfFailed(dxgiAdapter1.As(&dxgiAdapter4));
+	}
+	else
+	{
+		SIZE_T maxVRAM = 0;
+		for (UINT i = 0; i < dxgiFactory->EnumAdapters1(i, &dxgiAdapter1) != DXGI_ERROR_NOT_FOUND; ++i)
+		{
+
+		}
+	}
+};
+
+
 int main()
 {
 	return 0;
